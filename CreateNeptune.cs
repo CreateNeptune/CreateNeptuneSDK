8e347c6e0ce085d8f5fcb2755d992124be489240
Michael <mcos1231@gmail.com>
2022-05-27 15:47:02 -0400

    Added singletons

--- CreateNeptune.cs
+++ CreateNeptune.cs
@@ -1247,4 +1247,59 @@ namespace CreateNeptune
             "Bing","Caltech","Rice","Vandy","TheU","OSU","UM","Berkeley","UC","UT","UF","FIT","Parsons","Pratt","Tigers","Bulldogs","Zebras","MX","Dragons","STD",
             "Bengals","Cubs","Dodgers","Cards","Moscow","Cairo","Dubai","Berlin","Ajaccio","Munich","London","Okie","Newark","Charleston","Barca","Capetown"};
     }
+
+    /// <summary>
+    /// <para>Base class for singletons.</para>
+    /// <para>
+    /// OnAwake must be implemented and is called when the
+    /// first instance of this singleton is set.
+    /// </para>
+    /// <para>Example of declaring a child of this is included in the source code comments</para>
+    /// </summary>
+    /// <typeparam name="T">Always set this to the child type</typeparam>
+    public abstract class Singleton<T> : MonoBehaviour where T : class
+    {
+        /*
+        private class Example : Singleton<Example>
+        {
+            protected override void OnSuccessfulAwake()
+            {
+                Debug.Log("Started example singleton.");
+            }
+        }
+        */
+
+        public static T Instance;
+
+        private void Awake()
+        {
+            if (this is T self)
+            {
+                if (Instance == null)
+                {
+                    Instance = self;
+                    DontDestroyOnLoad(gameObject);
+                    OnSuccessfulAwake();
+                }
+                else if (Instance != self)
+                {
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                Debug.Log("You did not set an appropriate type for this singleton. See examples for details.");
+                Destroy(gameObject);
+            }
+        }
+
+        /// <summary>
+		/// This is called when just and only after the instance of the singleton is set.
+		/// Override this to implement functionality to occur at that moment.
+		/// </summary>
+        protected virtual void OnSuccessfulAwake()
+        {
+
+        }
+    }
 }
