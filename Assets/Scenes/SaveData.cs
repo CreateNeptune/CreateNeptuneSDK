a0f61aa5fedaead38233f749dfac215a0412029e
Miriam Suarez <mimi.g.suarez@gmail.com>
2022-11-11 15:13:44 -0500

    Added OnSuccessfulAwake example

--- Assets/Scenes/SaveData.cs
+++ Assets/Scenes/SaveData.cs
@@ -14,6 +14,13 @@ public class SaveData : SaveDataSingleton<SaveData, SerialSaveData>
 {
 	public static string[] pigNames;
 	public static float costPerPig;
+
+	protected override void OnSuccessfulAwake()
+	{
+		costPerPig = 10; // pricey guinea pigs
+		pigNames = new string[] { "Kiibow", "Teebow" };
+	}
+
 	protected override void Deserialize(SerialSaveData dataStruct)
 	{
 		SafeLoadDatum(ref pigNames, dataStruct.pigNames);
