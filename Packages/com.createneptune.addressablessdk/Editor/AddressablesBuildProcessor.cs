cbcfa1c192dccf0549c13281aa4902c541c166d2
Uti Azulay Mac <agustin.azulay@gmail.com>
2022-12-22 14:53:38 -0800

    Added toggeable addressables profile check

--- Packages/com.createneptune.addressablessdk/Editor/AddressablesBuildProcessor.cs
+++ Packages/com.createneptune.addressablessdk/Editor/AddressablesBuildProcessor.cs
@@ -9,13 +9,75 @@ namespace CreateNeptune.Addressables
 {
     public class AddressablesBuildProcessor
     {
-        private const string menuName = "Window/Asset Management/Addressables/Automatic Build";
-        private const string settingName = "autoaddressables";
-        private static bool IsEnabled
+        /// <summary>
+        /// AUTOMATIC BUILD
+        /// </summary>
+
+        private const string autoBuildMenuName = "Window/Asset Management/Addressables/Automatic Build";
+        private const string autoBuildSettingName = "autoaddressables";
+        private static bool BuildCheckIsEnabled
         {
-            get => EditorPrefs.GetBool(settingName, false);
-            set => EditorPrefs.SetBool(settingName, value);
+            get => EditorPrefs.GetBool(autoBuildSettingName, false);
+            set => EditorPrefs.SetBool(autoBuildSettingName, value);
         }
+
+        [MenuItem(autoBuildMenuName)]
+        private static void SetAutoAddressables()
+        {
+            // Add the auto-addressables setting to editor preferences if it doesn't already exist.
+            if (!EditorPrefs.HasKey(autoBuildSettingName))
+                EditorPrefs.SetBool(autoBuildSettingName, false);
+
+            // Alter the setting and set checked / unchecked in the menu.
+            BuildCheckIsEnabled = !BuildCheckIsEnabled;
+
+            Debug.Log("Automatic Addressables " + EditorPrefs.GetBool(autoBuildSettingName));
+        }
+
+        [MenuItem(autoBuildMenuName, true)]
+        private static bool SetAutoAddressablesValidate()
+        {
+            Menu.SetChecked(autoBuildMenuName, BuildCheckIsEnabled);
+
+            return true;
+        }
+
+        /// <summary>
+        /// PROFILE CHECK
+        /// </summary>
+
+        private const string profileSetMenuName = "Window/Asset Management/Addressables/Profile Set";
+        private const string profileSetSettingName = "profileaddressables";
+        private static bool ProfileCheckIsEnabled
+        {
+            get => EditorPrefs.GetBool(profileSetSettingName, false);
+            set => EditorPrefs.SetBool(profileSetSettingName, value);
+        }
+
+        [MenuItem(profileSetMenuName)]
+        private static void SetProfileCheck()
+        {
+            // Add the profile check setting to editor preferences if it doesn't already exist.
+            if (!EditorPrefs.HasKey(profileSetSettingName))
+                EditorPrefs.SetBool(profileSetSettingName, false);
+
+            // Set checked / unchecked in the menu.
+            ProfileCheckIsEnabled = !ProfileCheckIsEnabled;
+
+            Debug.Log("Addressables Profile Check" + EditorPrefs.GetBool(profileSetSettingName));
+        }
+
+        [MenuItem(profileSetMenuName, true)]
+        private static bool SetAddressablesProfileCheckValidate()
+        {
+            Menu.SetChecked(profileSetMenuName, ProfileCheckIsEnabled);
+
+            return true;
+        }
+
+        /// <summary>
+        /// BUILD PLAYER
+        /// </summary>
 
         [InitializeOnLoadMethod]
         private static void Initialize()
@@ -25,32 +87,17 @@ namespace CreateNeptune.Addressables
             BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayerHandler);
         }
 
-        [MenuItem(menuName)]
-        private static void SetAutoAddressables()
-        {
-            // Add the auto-addressables setting to editor preferences if it doesn't already exist.
-            if (!EditorPrefs.HasKey(settingName))
-                EditorPrefs.SetBool(settingName, false);
-
-            // Alter the setting and set checked / unchecked in the menu.
-            IsEnabled = !IsEnabled;
-
-            Debug.Log("Automatic Addressables " + EditorPrefs.GetBool(settingName));
-        }
-
-        [MenuItem(menuName, true)]
-        private static bool SetAutoAddressablesValidate()
-        {
-            Menu.SetChecked(menuName, IsEnabled);
-
-            return true;
-        }
-
         private static void BuildPlayerHandler(BuildPlayerOptions options)
         {
+            // Shown profile check (if enabled)
+            if (ProfileCheckIsEnabled)
+                SetProfile(EditorUtility.DisplayDialogComplex("Choose Addressables Profile",
+                    "Which Addressables Profile do you want to build with?\n(Must have profiles 'Remote' and 'Local')",
+                    "Remote", "Local", "Skip"));
+
             // If you prefer just having Addressables built no matter what, set
             // "Autobuild Addressables" under Window.
-            if (IsEnabled || EditorUtility.DisplayDialog("Build with Addressables",
+            if (BuildCheckIsEnabled || EditorUtility.DisplayDialog("Build with Addressables",
                 "Do you want to build clean Addressables before exporting?",
                 "Build with Addressables", "Skip"))
             {
@@ -59,6 +106,29 @@ namespace CreateNeptune.Addressables
 
             BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
         }
+
+        ///<summary>
+        /// Set the preferred profile for Addressables.
+        /// </summary>
+        private static void SetProfile(int dialogueChoice)
+        {
+            AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+
+            switch (dialogueChoice)
+            {
+                case 0:
+                    settings.BuildRemoteCatalog = true;
+                    settings.activeProfileId = settings.profileSettings.GetProfileId("Remote");
+                    break;
+                case 1:
+                    settings.BuildRemoteCatalog = false;
+                    settings.activeProfileId = settings.profileSettings.GetProfileId("Local");
+                    break;
+                case 2:
+                    break;
+            }
+        }
+
         ///<summary>
         /// Run a clean build before export.
         /// </summary>
